a6352b53919345e5cfd7e279b1949e5dd1b60819
sasaco <sasaicco@gmail.com>
2022-02-02 22:44:58 +0900

    lambda

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # frontistr-python
 FrontISTR を クラウドで用いる
+
+# [Lambda コンテナイメージの作成](https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/images-create.html#images-create-from-alt)
+
+1. docker build コマンドを使用して Docker イメージをビルドします。イメージの名前を入力します。次の例では、イメージに hello-world という名前を付けています。
+
+```
+docker build -t hello-world .    
+```
+
+2. Amazon ECR レジストリに対し、Docker CLI を認証します。
+
+```
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 123456789012.dkr.ecr.us-east-1.amazonaws.com    
+```
+
+3. create-repository コマンドを使用して、Amazon ECR にリポジトリを作成します。
+
+```
+aws ecr create-repository --repository-name hello-world --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
+```
+
+4. リポジトリ名と一致するタグをイメージに付け、docker push コマンドを使用してそのイメージを Amazon ECR にデプロイします。
+
+```
+docker tag  hello-world:latest 123456789012.dkr.ecr.us-east-1.amazonaws.com/hello-world:latest
+docker push 123456789012.dkr.ecr.us-east-1.amazonaws.com/hello-world:latest     
+```
--- app/app.py
+++ app/app.py
@@ -1,11 +1,55 @@
 import json
+import glob
+import subprocess
+import os
+import time
 
 def handler(event, context):
+
+    cnt_path = '/function/tmp/hinge.cnt'
+    mesh_path = '/function/tmp/hinge.mesh'
+    res_path = '/function/tmp/hinge.res.*'
+    vis_path = '/function/tmp/hinge_vis_*'
+
+    """
+    # 引数
+    if "body" in event:
+        js = json.loads(event["body"])
+    else:
+        js = event
+    cnt = js['cnt']
+    mesh = js['mesh']
+
+    # .cnt ファイルを 書き込む
+    fout=open(cnt_path, 'w')
+    print(cnt, file=fout)
+    fout.close()
+
+    # .mesh ファイルを 書き込む
+    fout=open(mesh_path, 'w')
+    print(mesh, file=fout)
+    fout.close()
+    """
+
+    # FrontISTR で 計算する
+    os.chdir('/function')
+    proc = subprocess.call('fistr1', shell=True)
+
+    # 書き込んだ .res ファイルを 読み込む
+    res = []
+    for r in glob.glob(res_path):
+        f = open(r)
+        res.append(f.read())  # ファイル終端まで全て読んだデータを返す
+        f.close()
+
+    # 返す
     return {
         "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
+        "headers": {
+            "Access-Control-Allow-Origin":"*",
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "res": res,
+        }),
     }
